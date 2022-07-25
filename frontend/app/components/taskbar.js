5c1b9897c92592e4108c71301e46bccb432a5716
senne <sennebels@gmail.com>
2022-07-25 11:18:27 +0200

    docs: :memo: innitititing  gitoop peeges

--- frontend/app/components/taskbar.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-// import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
-import { inject as service } from '@ember/service';
-
-export default class TaskbarComponent extends Component {
-  @service municipalities;
-  @tracked options = this.municipalities.data;
-}
